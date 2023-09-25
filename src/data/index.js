d0452cbb4041e46b3b0565d24b282fe39af7d93b
Phan Văn Hinh <phana2311@gmail.com>
2023-09-25 10:16:45 +0700

    nothing =))

--- src/data/index.js
+++ src/data/index.js
@@ -2,12 +2,12 @@ let data = {
     index: {
         title: 'Trang Chủ | Phan Hinh',
         posts: [
-            {
-                image: '/assets/images/Snowball-Edge.jpg',
-                title: '[A0016] Advanced Storage on AWS',
-                content: 'Tìm hiểu về AWS Snow Family, FSx, Storage Gateway, Transfer Family, Data Sync,...',
-                url: '/aws/16-advanced-storage-on-aws.html'
-            },
+            // {
+            //     image: '/assets/images/Snowball-Edge.jpg',
+            //     title: '[A0016] Advanced Storage on AWS',
+            //     content: 'Tìm hiểu về AWS Snow Family, FSx, Storage Gateway, Transfer Family, Data Sync,...',
+            //     url: '/aws/16-advanced-storage-on-aws.html'
+            // },
         ]
     },
     laravel: {
